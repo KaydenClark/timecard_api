c6b7338976e3a236c0a38c0bf764cb16cbf03afe
Kayden Clark <kaydenclark725@gmail.com>
2020-12-22 23:03:18 -0700

    framework for clocking

---
+++ DataAccess/time/clockOut.js
@@ -0,0 +1,30 @@
+const MongoClient = require('mongodb').MongoClient;
+// const ObjectId = require("mongodb").ObjectId;
+require('dotenv').config()
+
+const url = process.env.ATLAS_CONNECTION
+
+const dbName = 'TimeCard';
+const settings = {
+    useUnifiedTopology: true
+}
+
+const clockOut = () => {
+    console.log('connectiing...')
+    const iou = new Promise((resolve, reject) => {
+        // Use connect method to connect to the server
+        MongoClient.connect(url, settings, function (err, client) {
+            if(err){
+                reject(err)
+            } else {
+                const db = client.db(dbName);
+                client.close();
+                resolve("Connected successfully to server")
+                console.log("Connected to Server")
+            }
+        });
+    })
+    return iou
+}
+
+module.exports = {clockOut()}
